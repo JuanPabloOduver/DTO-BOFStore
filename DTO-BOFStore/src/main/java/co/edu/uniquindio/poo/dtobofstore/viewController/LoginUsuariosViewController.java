219ccbf93453ffd020b81ed1a96867034cc02a51
Camilo-Mendoza <camilodelcastillo321@gmail.com>
2025-03-29 23:07:02 -0500

    viewController

---
+++ DTO-BOFStore/src/main/java/co/edu/uniquindio/poo/dtobofstore/viewController/LoginUsuariosViewController.java
@@ -0,0 +1,62 @@
+package co.edu.uniquindio.poo.dtobofstore.viewController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+
+public class LoginUsuariosViewController {
+
+
+        @FXML
+        private Button btn_registrarse;
+
+        @FXML
+        private Text lbl_bienvenido;
+
+        @FXML
+        private AnchorPane ap_usuario;
+
+        @FXML
+        private Text lbl_iniciarComo;
+
+        @FXML
+        private TextField txt_Contraseña;
+
+        @FXML
+        private Button btn_registrarse1;
+
+        @FXML
+        private TextField txt_nombre;
+
+
+
+        @FXML
+        void onRegistrarse(ActionEvent event) {
+
+        }
+
+
+
+        @FXML
+        void onContraseña(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onNombre(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onRegistrarse1(ActionEvent event) {
+
+        }
+
+
+
+
+
+}
